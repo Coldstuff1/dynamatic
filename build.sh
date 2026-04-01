3971416b9c7bdccf1ebbc52cf6341b74d459c16c
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2026-03-31 20:17:46 -0400

    builds!

--- build.sh
+++ build.sh
@@ -169,7 +169,8 @@ if should_run_cmake ; then
       -DLLVM_TARGETS_TO_BUILD="host" \
       -DCMAKE_BUILD_TYPE=Release \
       -DLLVM_ENABLE_ASSERTIONS=ON \
-      -DCMAKE_CXX_FLAGS="-include cstdint" \
+      -DCMAKE_CXX_FLAGS="-include cstdint -Wno-c2y-extensions" \
+      -DCMAKE_C_FLAGS="-Wno-c2y-extensions" \
       $CMAKE_FLAGS_LLVM
   exit_on_fail "Failed to cmake polygeist/llvm-project"
 fi
@@ -223,7 +224,8 @@ if should_run_cmake ; then
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DLLVM_ENABLE_ASSERTIONS=ON \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-      -DCMAKE_CXX_FLAGS="-include cstdint" \
+      -DCMAKE_CXX_FLAGS="-include cstdint -Wno-c2y-extensions" \
+      -DCMAKE_C_FLAGS="-Wno-c2y-extensions" \
       $CMAKE_FLAGS_LLVM
   exit_on_fail "Failed to cmake circt/llvm"
 fi
@@ -259,9 +261,9 @@ if should_run_cmake ; then
       -DIVERILOG_DISABLE=ON \
       -DYOSYS_DISABLE=ON \
       -DCAPNP_DISABLE=ON \
-      -DOR_TOOLS_DISABLE=ON \
       -DCAPNP_DISABLE=ON \
-      -DCMAKE_CXX_FLAGS="-include cstdint" \
+      -DCMAKE_CXX_FLAGS="-include cstdint -Wno-c2y-extensions -DOR_PROTO_DLL=" \
+      -DCMAKE_C_FLAGS="-Wno-c2y-extensions" \
       $CMAKE_FLAGS_SUPER
   exit_on_fail "Failed to cmake circt"
 fi
@@ -289,7 +291,8 @@ if should_run_cmake ; then
       -DLLVM_ENABLE_ASSERTIONS=ON \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       -DDYNAMATIC_BUILD_VISUALIZER=$BUILD_VISUALIZER \
-      -DCMAKE_CXX_FLAGS="-include cstdint" \
+      -DCMAKE_CXX_FLAGS="-include cstdint -Wno-c2y-extensions" \
+      -DCMAKE_C_FLAGS="-Wno-c2y-extensions" \
       $CMAKE_FLAGS_SUPER
   exit_on_fail "Failed to cmake dynamatic"
 fi
